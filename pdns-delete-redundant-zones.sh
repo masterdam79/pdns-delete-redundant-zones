fecffc64db8755a7eb965686eb9264a2c3f73b1a
Richard Reijmers <masterdam@scaleop2.hs1.lan>
2016-07-03 22:27:39 +0200

    ust a bet

--- pdns-delete-redundant-zones.sh
+++ pdns-delete-redundant-zones.sh
@@ -61,7 +61,7 @@ for DOMAIN in ${DOMAINS}; do
       ECHOBLUE ${NSRECORD::-1}
       echo "${DOMAIN} - ${NSRECORD::-1}" >> /root/log/${DATABASE}-${TIMESTAMP}-$0.log
       if [[ ${NSRECORD::-1} == ${HOSTNAME} ]]; then
-        ECHOSILVER "${NSRECORD::-1} == ${HOSTNAME}"
+        ECHOSILVER "${NSRECORD::-1} is ${HOSTNAME}"
         # If current NS record contains the hostname
         NOTDELETING="true"
       else
