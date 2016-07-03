5ff04ffdd4106c8dccd5a7a0aac67cf2bbfc41fd
Richard Reijmers <masterdam@scaleop2.hs1.lan>
2016-07-03 20:39:06 +0200

    mysqldump

--- pdns-delete-redundant-zones.sh
+++ pdns-delete-redundant-zones.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 HOSTNAME=$(hostname -f)
-
+DATE=$(date +"%Y-%m-%d")
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+DATABASE="powerdns"
 # Make backup of the powerdns database
+mysqldump ${DATABASE} > /root/${DATABASE}-${TIMESTAMP}-$0.sql
 
 # Get list of domains on this server.
